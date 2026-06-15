9aadf4fcbffc428ca296b25c9ee14d8fa04b8915
SALONI <116374603+CodeWithSaloni@users.noreply.github.com>
2026-06-15 20:55:31 +0530

    Added Employee Management System Version 1

--- EmployeeManagementSystem.cs
+++ EmployeeManagementSystem.cs
@@ -1,1 +1,39 @@
-Added Employee Management System Version 1
+using System;
+using System.Collections.Generic;
+
+public class Employee
+{
+    public int EmpId;
+    public string EmpName;
+    public decimal Salary;
+    public int DeptId;
+}
+
+public class Program
+{
+    static List<Employee> employees = new List<Employee>();
+
+    public static void Main()
+    {
+        Employee emp1 = new Employee();
+
+        emp1.EmpId = 1;
+        emp1.EmpName = "Saloni";
+        emp1.Salary = 25000;
+        emp1.DeptId = 1;
+
+        employees.Add(emp1);
+
+        Console.WriteLine("Employee List");
+
+        foreach(var emp in employees)
+        {
+            Console.WriteLine(
+                "ID: " + emp.EmpId +
+                " | Name: " + emp.EmpName +
+                " | Salary: " + emp.Salary +
+                " | DeptId: " + emp.DeptId
+            );
+        }
+    }
+}
